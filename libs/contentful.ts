20132d501ccbbf7edd48174945283f8fd2483812
hirokoino <ino@16bit.co.jp>
2020-12-19 21:55:08 +0900

    環境変数変更

--- libs/contentful.ts
+++ libs/contentful.ts
@@ -1,4 +1,4 @@
 export const client = require('contentful').createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
 })
