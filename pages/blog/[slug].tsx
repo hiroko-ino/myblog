ec2e0e9beb5ade6b964e2b57711b5acb83cba637
hirokoino <ino@16bit.co.jp>
2021-01-03 22:28:36 +0900

    シェアボタン実装完了

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -9,7 +9,17 @@ import Layout from '../../components/Layout'
 
 import styled from './[slug].module.scss'
 
-const Blog = ({ post, category }) => {
+import {
+  FacebookShareButton,
+  HatenaShareButton,
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookIcon,
+  HatenaIcon,
+} from "react-share";
+
+
+const Blog = ({ post, category, slug }) => {
   const day = dayjs(post.sys.createdAt);
 
   return (
@@ -25,6 +35,17 @@ const Blog = ({ post, category }) => {
             <div className={styled.main}>
                 <h1 className={styled.heading}>{post.fields.title}</h1>
                 <p className={styled.category}>{'{'} category: {`"${post.fields.category.fields.name}"`} {'}'}</p>
+                <div className={styled.share}>
+                  <TwitterShareButton url={`https://type-any.com/blog/${slug}`} title={post.fields.title}>
+                    <TwitterIcon size={32} round={true} />
+                  </TwitterShareButton>
+                  <FacebookShareButton url={`https://type-any.com/blog/${slug}`} quote={post.fields.title}>
+                    <FacebookIcon size={32} round={true} />
+                  </FacebookShareButton>
+                  <HatenaShareButton url={`https://type-any.com/blog/${slug}`} title={post.fields.title}>
+                    <HatenaIcon size={32} round={true} />
+                  </HatenaShareButton>
+                </div>
                 <div className={styled.body} dangerouslySetInnerHTML={{ __html: post.fields.body}} />
                 <div className={styled.back}><Link href="/"><a className={styled.backlink}>BackToIndex</a></Link></div>
             </div>
@@ -52,7 +73,8 @@ export const getStaticProps = async ({ params }) => {
   return {
     props: {
       post: post.items[0],
-      category: category.items
+      category: category.items,
+      slug: params.slug
     },
   }
 }
