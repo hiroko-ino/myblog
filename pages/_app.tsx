d43c370d5a013916e350d3dec3137518da59530e
hirokoino <ino@16bit.co.jp>
2021-01-30 19:03:10 +0900

    typescriptエラー解消

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import 'reset-css'
 import '../styles/globals.scss'
+import { AppProps /*, AppContext */ } from 'next/app';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
