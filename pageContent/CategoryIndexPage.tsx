28e176fbf31ce36436f810ba3b042a66e798e476
hirokoino <ino@16bit.co.jp>
2021-12-12 00:16:37 +0900

    誤り修正

--- pageContent/CategoryIndexPage.tsx
+++ pageContent/CategoryIndexPage.tsx
@@ -10,6 +10,7 @@ interface CategoryIndexPageProps {
 }
 
 const CategoryIndexPage: React.FC<CategoryIndexPageProps> = React.memo(({ posts, category, categorySlug }) => {
+  console.log(posts)
   return (
     <Layout category={category}>
       <div>
