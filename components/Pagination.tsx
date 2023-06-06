25a11826aaf094849225907fe3d036e09f9496ad
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2023-06-06 22:10:21 +0900

    Link legacyBehavior

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -14,7 +14,7 @@ const Pagination = ({posts, currentNum, category = null}: {
         className={[styled.item, currentNum === i + 1 && styled.is_active].join(' ')}>
           {currentNum === i + 1 ?
             i + 1 :
-            <Link href={category ? `/category/${category}/page/${i + 1}` : `/blog/page/${i + 1}`}><a className={styled.link}>{i + 1}</a></Link>}</li>
+            <Link legacyBehavior href={category ? `/category/${category}/page/${i + 1}` : `/blog/page/${i + 1}`}><a className={styled.link}>{i + 1}</a></Link>}</li>
       );
     }
     return list;
